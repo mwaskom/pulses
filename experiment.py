912bb5fb5d3908cc1839c6456fd68a09ee1ea03c
mwaskom <mwaskom@stanford.edu>
2016-09-26 13:20:56 -0400

    Remove old unneeded code

--- experiment.py
+++ experiment.py
@@ -73,14 +73,8 @@ def experiment_loop(p, win, stims, tracker):
     stim_event.tracker = tracker
 
     # Initialize the experiment log
-    # We shouldn't need to duplicate this information, and we won't with a
-    # different approach to maintaining the log
     # TODO we need to add all the columns
-    log_cols = ["stim_onset", "resp_onset", "pulse_count",
-                "obs_mean_l", "obs_mean_r", "obs_mean_delta",
-                "key", "response", "response_during_stim",
-                "gen_correct", "obs_correct", "rt",
-                "dropped_frames"]
+    log_cols = []
 
     log = cregg.DataLog(p, log_cols)
 
@@ -93,72 +87,24 @@ def experiment_loop(p, win, stims, tracker):
     rng = np.random.RandomState()
     stims["patches"].rng = rng
 
-    with cregg.PresentationLoop(win, p, log, fix=stims["fix"],
+    with cregg.PresentationLoop(win, p, log,
+                                fix=stims["fix"],
                                 tracker=tracker,
                                 feedback_func=show_performance_feedback,
                                 exit_func=experiment_exit):
 
         stim_event.clock.reset()
 
-        for t, t_info in design.iterrows():
+        # Loop over trials
+        for t, t_info in generate_trials(p, stim_event.clock):
 
-            if t_info["break"]:
-
-                # Show a progress bar and break message
-                stims["progress"].update_bar(t / len(design))
-                stims["progress"].draw()
-                stims["break"].draw()
-
-            # Set up a random number generator for this trial
-            trial_rng = np.random.RandomState(t_info["random_seed"])
-            stims["patches"].rng = trial_rng
-
-            # Build the pulse schedule for this trial
-            trial_flips = win.refresh_hz * t_info["trial_dur"]
-            pulse_flips = win.refresh_hz * p.pulse_duration
-
-            # Schedule pulse onsets
-            trial_onsets = pulse_onsets(p, win.refresh_hz,
-                                        trial_flips, trial_rng)
-            t_info.ix["pulse_count"] = len(trial_onsets)
-
-            # Determine the sequence of stimulus contrast values
-            trial_contrast = np.zeros((trial_flips, 2))
-            trial_contrast_means = []
-            trial_contrast_values = []
-            for i, mean in enumerate(t_info[["gen_mean_l", "gen_mean_r"]]):
-                vector, values = contrast_schedule(trial_onsets,
-                                                   mean,
-                                                   p.contrast_sd,
-                                                   p.contrast_limits,
-                                                   trial_flips,
-                                                   pulse_flips,
-                                                   trial_rng)
-                trial_contrast[:, i] = vector
-                trial_contrast_means.append(np.mean(values))
-                trial_contrast_values.append(values)
-
-            # Log some information about the actual values
-            obs_mean_l, obs_mean_r = trial_contrast_means
-            t_info.ix["obs_mean_l"] = obs_mean_l
-            t_info.ix["obs_mean_r"] = obs_mean_r
-            t_info.ix["obs_mean_delta"] = obs_mean_r - obs_mean_l
-
-            # Log the pulse-wise information
-            log.pulses.update(trial_onsets, trial_contrast_values)
-
-            # Compute the signed difference in the generating means
-            contrast_delta = t_info["gen_mean_r"] - t_info["gen_mean_l"]
+            # Generate the pulse train for this trial
+            p_info = make_pulse_train(p, t_info)
 
             # Execute this trial
-            res = stim_event(t_info, trial_contrast, contrast_delta)
-
-            # Log whether the response agreed with what was actually shown
-            res["obs_correct"] = (res["response"] ==
-                                  (t_info["obs_mean_delta"] > 0))
+            t_info = stim_event(t_info, p_info)
 
             # Record the result of the trial
-            t_info = t_info.append(pd.Series(res))
             log.add_data(t_info)
 
         # Show the final fixation to allow hemodynamics to return to baseline
@@ -188,30 +134,6 @@ def experiment_exit(log):
             os.system("open " + png_fname)
 
 
-def plot_performance(df, fname):
-
-    import matplotlib as mpl
-    mpl.use("Agg")
-    import seaborn as sns
-    import matplotlib.pyplot as plt
-
-    f, axes = plt.subplots(1, 2, figsize=(8, 4))
-
-    # Plot the psychometric function
-    if df.response.notnull().any():  # Needed due to matplotlib 1.4 bug
-        unsigned_delta = df.gen_mean_delta.abs()
-        sns.pointplot(x=unsigned_delta, y="gen_correct", data=df, ax=axes[0])
-
-    # Plot the difference in scheduled and achieved stim onset
-    axes[1].plot(df.stim_time, df.stim_time - df.stim_onset, marker="o")
-    axes[1].set(xlabel="Stim time (s)", ylabel="Stim timing error (s)")
-    axes[1].axhline(y=0, c=".5", lw=2, dashes=(4, 1.5))
-
-    f.tight_layout()
-    f.savefig(fname)
-    plt.close(f)
-
-
 def save_pulse_log(log):
 
     if not log.p.nolog:
@@ -220,7 +142,12 @@ def save_pulse_log(log):
 
 
 # =========================================================================== #
-# Event controller
+# Design functions
+# =========================================================================== #
+
+
+# =========================================================================== #
+# Trial controller
 # =========================================================================== #
 
 
@@ -399,21 +326,6 @@ class TrialEngine(object):
 
     def __call__(self, t_info, p_info):
         """Execute a stimulus event."""
-        # Initialize the trial result data
-        # TODO move into trial generator
-        res = pd.Series(dict(
-            fix_onset=np.nan,
-            targ_onset=np.nan,
-            crit_onset=np.nan,
-            resp_onset=np.nan,
-            correct=False,
-            response=np.nan,
-            rt=np.nan,
-            answered=False,
-            eye_response=False,
-            key_response=False,
-            key=np.nan,
-        ))
 
         # Inter-trial-interval
         self.fix.color = self.fix.iti_color
@@ -526,201 +438,6 @@ class TrialEngine(object):
         self.win.flip()
 
         return t_info
-
-
-# =========================================================================== #
-# Stimulus log control
-# =========================================================================== #
-
-
-class PulseLog(object):
-
-    def __init__(self):
-
-        self.pulse_times = []
-        self.contrast_values = []
-
-    def update(self, pulse_times, contrast_values):
-
-        self.pulse_times.append(pulse_times)
-        self.contrast_values.append(contrast_values)
-
-    def save(self, fname):
-
-        np.savez(fname,
-                 pulse_onsets=self.pulse_times,
-                 contrast_values=self.contrast_values)
-
-
-# =========================================================================== #
-# Design functions
-# =========================================================================== #
-
-
-def generate_contrast_pairs(deltas, p):
-    """Find valid pairs of contrasts (or distribution means) given deltas."""
-    rng = np.random.RandomState()
-    deltas = np.asarray(deltas)
-    contrasts = np.zeros((len(deltas), 2))
-    replace = np.ones(len(deltas), np.bool)
-
-    while replace.any():
-
-        # Determine the "pedestal" contrast
-        # Note that this is not a fully correct use of this term
-        pedestal = cregg.flexible_values(p.contrast_pedestal,
-                                         replace.sum(), rng)
-
-        # Determine the two stimulus contrasts
-        contrasts[replace] = np.c_[pedestal - deltas[replace] / 2,
-                                   pedestal + deltas[replace] / 2]
-
-        # Check for invalid pairs
-        replace = ((contrasts.min(axis=1) < p.contrast_limits[0])
-                   | (contrasts.max(axis=1) > p.contrast_limits[1]))
-
-    return contrasts
-
-
-def generate_run_design(p):
-
-    cycle_data = []
-
-    # Build sets of trial schedules representing each signed stimulus strength
-    for _ in range(p.cycles_per_run - p.cycles_repeated):
-
-        # Everything is structured around the vector of signed deltas
-        unsigned_deltas = np.array(p.contrast_deltas)
-        signed_deltas = np.concatenate([-unsigned_deltas, unsigned_deltas])
-        trials_per_cycle = len(signed_deltas)
-
-        # Determine the contrasts for each trial
-        contrasts = generate_contrast_pairs(signed_deltas, p)
-        column_names = ["gen_mean_l", "gen_mean_r"]
-        cycle_df = pd.DataFrame(contrasts, columns=column_names)
-        cycle_df["gen_mean_delta"] = signed_deltas
-
-        # Determine the duration of each trial
-        trial_dur = cregg.flexible_values(p.trial_dur, trials_per_cycle)
-        cycle_df["trial_dur"] = trial_dur
-
-        # Determine a fixed seed for each cycle condition
-        seed_base = np.random.randint(1000, 9000)
-        seed = (seed_base + 10000 * signed_deltas).astype(np.int)
-        cycle_df["random_seed"] = seed
-
-        # Add a columne identifying if these trials are "paired"
-        cycle_df["paired_trial"] = False
-
-        # Add in some timing information we want kept constant
-        cycle_df["pre_stim_dur"] = cregg.flexible_values(p.pre_stim_dur,
-                                                         trials_per_cycle)
-        cycle_df["post_stim_dur"] = cregg.flexible_values(p.post_stim_dur,
-                                                          trials_per_cycle)
-        cycle_df["resp_dur"] = cregg.flexible_values(p.resp_dur,
-                                                     trials_per_cycle)
-        cycle_df["feedback_dur"] = cregg.flexible_values(p.feedback_dur,
-                                                         trials_per_cycle)
-
-        cycle_data.append(cycle_df)
-
-    # Duplicate sets so that we have some identical trials
-    for i in range(p.cycles_repeated):
-        cycle_data[i]["paired_trial"] = True
-        cycle_data.insert(0, cycle_data[i])
-
-    # Concatenate sets and randomize trial order
-    run_df = (pd.concat(cycle_data)
-                .sample(frac=1, replace=False)
-                .reset_index(drop=True))
-    n_trials = len(run_df)
-
-    # Add in breaks
-    run_df["break"] = ~(run_df.index.values % p.trials_per_break).astype(bool)
-    run_df.loc[0, "break"] = False
-
-    # Determine the full length of each trial
-    trial_seconds = (run_df["pre_stim_dur"]
-                     + run_df["trial_dur"]
-                     + run_df["post_stim_dur"]
-                     + run_df["resp_dur"]
-                     + run_df["feedback_dur"])
-    total_seconds = trial_seconds.sum()
-
-    # Generate ITIs to make the run duration what we expect
-    satisfied = False
-    while not satisfied:
-        iti = cregg.flexible_values(p.iti_dur, n_trials)
-        if p.seconds_per_run is None:
-            needed_seconds = 0
-        else:
-            needed_seconds = p.seconds_per_run - total_seconds
-            needed_seconds -= iti.sum()
-            iti += needed_seconds / n_trials
-        if iti.min() > 1:  # XXX needs parameterization
-            satisfied = True
-    run_df["iti"] = iti
-    trial_seconds += iti
-
-    # Schedule the onset of each stimulus
-    run_df["stim_time"] = trial_seconds.cumsum().shift(1).fillna(0) + iti
-
-    # TODO fix nomenclature so this is less confusing
-    run_df["full_trial_dur"] = trial_seconds
-
-    return run_df
-
-
-def pulse_onsets(p, refresh_hz, trial_flips, rng=None):
-    """Return indices for frames where each pulse will start."""
-    if rng is None:
-        rng = np.random.RandomState()
-
-    # Convert seconds to screen refresh units
-    pulse_secs = cregg.flexible_values(p.pulse_duration, random_state=rng)
-    # TODO this won't handle stochastic pulse durations properly
-    pulse_flips = refresh_hz * pulse_secs
-
-    # Schedule the first pulse for the trial onset
-    pulse_times = [0]
-
-    # Schedule additional pulses
-    while True:
-
-        last_pulse = pulse_times[-1]
-        ipi = cregg.flexible_values(p.pulse_gap, random_state=rng)
-        ipi_flips = int(np.round(ipi * refresh_hz))
-        next_pulse = (last_pulse +
-                      pulse_flips +
-                      ipi_flips)
-        if (next_pulse + pulse_flips) > trial_flips:
-            break
-        else:
-            pulse_times.append(int(next_pulse))
-
-    pulse_times = np.array(pulse_times, np.int)
-
-    return pulse_times
-
-
-def contrast_schedule(onsets, mean, sd, limits,
-                      trial_flips, pulse_flips, rng=None):
-    """Return a vector with the contrast on each flip."""
-    if rng is None:
-        rng = np.random.RandomState()
-
-    contrast_vector = np.zeros(trial_flips)
-    contrast_values = []
-    for onset in onsets:
-        offset = onset + pulse_flips
-        while True:
-            pulse_contrast = rng.normal(mean, sd)
-            if limits[0] <= pulse_contrast <= limits[1]:
-                break
-        contrast_vector[onset:offset] = pulse_contrast
-        contrast_values.append(pulse_contrast)
-
-    return contrast_vector, contrast_values
 
 
 if __name__ == "__main__":
