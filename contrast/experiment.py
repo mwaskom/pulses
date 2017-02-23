ba30c2472ba0028b13570296d6fd3b687c298bbd
mwaskom <mwaskom@stanford.edu>
2017-02-23 16:39:37 -0500

    Begin rewriting contrast discrimination experiment in visigoth framework

---
+++ contrast/experiment.py
@@ -0,0 +1,173 @@
+import itertools
+
+import numpy as np
+import pandas as pd
+
+from visigoth.tools import AcquireFixation, AcquireTarget, flexible_values
+from visigoth.stimuli import Point, Points, Pattern, GaussianNoise
+
+
+def create_stimuli(exp):
+
+    # Fixation point
+    fix = Point(exp.win,
+                exp.p.fix_radius,
+                exp.p.fix_trial_color)
+
+    # Saccade targets
+    targets = Points(exp.win,
+                     exp.p.target_pos,
+                     exp.p.target_radius,
+                     exp.p.target_color)
+
+    # Gaussian noise field
+    noise = GaussianNoise(exp.win,
+                          mask=exp.p.noise_mask,
+                          size=exp.p.stim_size,
+                          pix_per_deg=exp.p.noise_resolution)
+
+    # Average of multiple sinusoidal grating stimulus
+    pattern = Pattern(exp.win,
+                      n=exp.p.stim_gratings,
+                      elementTex=exp.p.stim_tex,
+                      elementMask=exp.p.stim_mask,
+                      sizes=exp.p.stim_size,
+                      sfs=exp.p.stim_sf,
+                      pos=(0, 0)
+                      )
+
+    return dict(fix=fix, targets=targets, pattern=pattern, noise=noise)
+
+
+def generate_trials(exp):
+    """Yield trial and pulse train info."""
+
+    # Create an infinite iterator for trial data
+    for t in itertools.count(1):
+
+        # Get the current time
+        now = exp.clock.getTime()
+
+        # Schedule the next trial
+        wait_iti = flexible_values(exp.p.wait_iti)
+
+        # Determine the stimulus parameters for this trial
+        gen_dist = flexible_values(list(range(exp.p.dist_means)))
+        gen_mean = exp.p.dist_means[gen_dist]
+        gen_sd = exp.p.dist_sds[gen_dist]
+        target = exp.p.dist_targets[gen_dist]
+
+        trial_info = dict(
+
+            # Basic trial info
+            subject=exp.p.subject,
+            session=exp.p.session,
+            run=exp.p.run,
+            trial=t,
+
+            # Stimulus parameters
+            gen_dist=gen_dist,
+            gen_mean=gen_mean,
+            gen_sd=gen_sd,
+            target=target,
+
+            # Pulse info (filled in below)
+            pulse_count=np.nan,
+            pulse_train_dur=np.nan,
+
+            # Timing parameters
+            wait_iti=wait_iti,
+            wait_pre_stim=flexible_values(exp.p.wait_pre_stim),
+            wait_resp=flexible_values(exp.p.wait_resp),
+            wait_feedback=flexible_values(exp.p.wait_feedback),
+
+            # Achieved timing data
+            onset_fix=np.nan,
+            onset_targ=np.nan,
+            onset_stim=np.nan,
+            onset_resp=np.nan,
+            onset_feedback=np.nan,
+
+            # Subject response fields
+            result=np.nan,
+            responded=False,
+            response=np.nan,
+            correct=np.nan,
+            rt=np.nan,
+            stim_blink=np.nan,
+
+        )
+
+        t_info = pd.Series(trial_info, dtype=np.object)
+        p_info = generate_pulse_train(exp, t_info)
+
+        t_info["pulse_count"] = len(p_info)
+        t_info["pulse_train_dur"] = (p_info["gap_dur"].sum()
+                                     + p_info["pulse_dur"].sum())
+
+        expected_trial_dur = (t_info["wait_pre_stim"]
+                              + t_info["wait_pre_stim"]
+                              + t_info["pulse_train_dur"]
+                              + exp.p.wait_feedback,
+                              + 2)  # Account for fix/response delay
+
+        if (now + expected_trial_dur) > exp.p.run_duration:
+            raise StopIteration
+
+        yield t_info, p_info
+
+
+def generate_pulse_train(exp, t_info):
+    """Generate the pulse train for a given trial."""
+    rng = np.random.RandomState()
+
+    # Randomly sample the pulse count for this trial
+    if rng.rand() < p.pulse_single_prob:
+        # Special case single pulse trials
+        count = 1
+    else:
+        count = flexible_values(exp.p.pulse_count, 1, rng,
+                                max=exp.p.pulse_count_max)
+
+    # Account for the duration of each pulse
+    pulse_dur = flexible_values(exp.p.pulse_dur, count, rng)
+    total_pulse_dur = np.sum(pulse_dur)
+
+    # Randomly sample gap durations with a constraint on trial duration
+    train_dur = np.inf
+    while train_dur > exp.p.pulse_train_max:
+        gap_dur = flexible_values(exp.p.pulse_gap, count, rng)
+        train_dur = np.sum(gap_dur) + total_pulse_dur
+
+    # Generate the stimulus strength for each pulse
+    max_contrast = 1 / np.sqrt(exp.p.stim_gratings)
+    contrast_dist = "norm", t_info["gen_mean"], t_info["gen_sd"]
+    log_contrast = flexible_values(contrast_dist, count, rng,
+                                   max=np.log10(max_contrast))
+    contrast = 10 ** log_contrast
+
+    p_info = pd.DataFrame(dict(
+
+        # Basic trial information
+        subject=exp.p.subject,
+        session=exp.p.session,
+        run=exp.p.run,
+        trial=t_info["trial"],
+
+        # Pulse information
+        pulse=np.arange(1, count + 1),
+        log_contrast=log_contrast,
+        contrast=contrast,
+        gap_dur=gap_dur,
+        pulse_dur=pulse_dur,
+
+        # Intitialize fields to track achieved performance
+        occurred=False,
+        blink=False,
+        onset_time=np.nan,
+        offset_time=np.nan,
+        dropped_frames=np.nan,
+
+    ))
+
+    return p_info
---
+++ contrast/params.py
@@ -0,0 +1,65 @@
+
+yellow = (.8, .6, -.8)
+
+base = dict(
+
+    display_name="laptop",
+    display_luminance=35,
+
+    fix_radius=.15,
+    fix_window=2,
+
+    fix_iti_color=None,
+    fix_ready_color=yellow,
+    fix_trial_color=yellow,
+
+    target_pos=[(-8, 4), (8, 4)],
+    target_color=yellow,
+    target_radius=.25,
+    target_window=5,
+
+    monitor_eye=True,
+    monitor_key=False,
+
+    eye_simulate=True,
+    eye_fixbreak_timeout=.5,
+    eye_blink_timeout=.5,
+    eye_target_wait=.25,
+    eye_target_hold=.25,
+
+    dist_means=[-.8, -1.2],
+    dist_sds=[.15, .15],
+    dist_targets=[0, 1],
+
+    stim_pos=[(-5.6, -2.0), (5.6, -2.0)],
+    stim_radius=3,
+    stim_sf=3,
+    stim_tex="sin",
+    stim_mask="raisedCos",
+    stim_size=6,
+    stim_gratings=8,
+
+    noise_mask="circle",
+    noise_contrast=.1,
+    noise_opacity=.5,
+    noise_resolution=20,
+    noise_hz=5,
+
+    wait_iti=1,
+    wait_fix=5,
+    wait_pre_stim=1,
+    wait_resp=5,
+    wait_feedback=.5,
+
+    pulse_count=("geom", .5, 1),
+    pulse_count_max=5,
+    pulse_single_prob=.1,
+    pulse_dur=.2,
+    pulse_gap=("truncexpon", (8 - 2) / 3, 2, 3),
+    pulse_train_max=16,
+
+    run_duration=540,
+
+    output_template="data/{subject}/{session}/{paramset}_{time}",
+
+)
