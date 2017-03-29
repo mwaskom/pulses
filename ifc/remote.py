fe25e72cd272bb5b5e49942e1f671b225a63fd7a
mwaskom <mwaskom@stanford.edu>
2017-03-29 10:18:43 -0400

    Link remote objects to params

--- ifc/remote.py
+++ ifc/remote.py
@@ -3,12 +3,12 @@ import matplotlib as mpl
 
 def create_stim_artists(remote):
 
-    noise = mpl.patches.Circle((0, 0),
+    noise = mpl.patches.Circle(remote.exp.p.stim_pos,
                                remote.p.stim_size / 2,
                                fc="k", lw=0, alpha=.25,
                                animated=True)
 
-    pattern = mpl.patches.Circle((0, 0),
+    pattern = mpl.patches.Circle(remote.exp.p.stim_pos,
                                  remote.p.stim_size / 2,
                                  fc="r", lw=0,
                                  animated=True)
