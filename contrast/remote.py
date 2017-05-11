be978cdd1816bd00f2e7f088bce89be83b0a2829
mwaskom <mwaskom@stanford.edu>
2017-05-11 17:18:28 -0400

    Remove noise object from remote

--- contrast/remote.py
+++ contrast/remote.py
@@ -3,14 +3,10 @@ import matplotlib as mpl
 
 def create_stim_artists(remote):
 
-    noise = mpl.patches.Circle((0, 0),
-                               remote.p.stim_size / 2,
-                               fc="k", lw=0, alpha=.25,
-                               animated=True)
-
     pattern = mpl.patches.Circle((0, 0),
                                  remote.p.stim_size / 2,
                                  fc="r", lw=0,
+                                 alpha=.5,
                                  animated=True)
 
-    return dict(noise=noise, pattern=pattern)
+    return dict(pattern=pattern)
