f3fb43a0b91ff641f4d5303f1cb5b0f0995336cf
Michael Waskom <mwaskom@nyu.edu>
2018-09-27 14:40:45 -0400

    Better variable name

--- fmri/design.py
+++ fmri/design.py
@@ -5,25 +5,25 @@ from visigoth.ext.bunch import Bunch
 from visigoth.tools import flexible_values
 
 
-def generate_run(d, p, rng=None):
+def generate_run(contraints, p, rng=None):
 
     if rng is None:
         rng = np.random.RandomState()
 
-    n_trials = d.trials_per_run
+    n_trials = constraints.trials_per_run
 
     # --- Assign trial components
 
     # Assign the stimulus to a side
 
     stim_pos = np.repeat([0, 1], n_trials // 2)
-    while max_repeat(stim_pos) > d["max_stim_repeat"]:
+    while max_repeat(stim_pos) > contraints.max_stim_repeat:
         stim_pos = rng.permutation(stim_pos)
 
     # Assign the target to a side
 
     gen_dist = np.repeat([0, 1], n_trials // 2)
-    while max_repeat(gen_dist) > d["max_dist_repeat"]:
+    while max_repeat(gen_dist) > contraints.max_dist_repeat:
         gen_dist = rng.permutation(gen_dist)
 
     # Assign pulse counts to each trial
@@ -33,7 +33,7 @@ def generate_run(d, p, rng=None):
     expected_count_dist = count_pmf * n_trials
 
     count_error = np.inf
-    while count_error > d.sum_count_error:
+    while count_error > constraints.sum_count_error:
 
         pulse_count = flexible_values(p.pulse_count, n_trials, rng,
                                       max=p.pulse_count_max).astype(int)
@@ -44,7 +44,7 @@ def generate_run(d, p, rng=None):
     # Assign initial ITI to each trial
 
     total_iti = np.inf
-    while not_in_range(total_iti, d.iti_range):
+    while not_in_range(total_iti, constraints.iti_range):
         wait_iti = flexible_values(p.wait_iti, n_trials, rng)
         if p.skip_first_iti:
             wait_iti[0] = 0
@@ -78,7 +78,7 @@ def generate_run(d, p, rng=None):
     # Assign gaps between pulses
 
     run_duration = np.inf
-    while not_in_range(run_duration, d.run_range):
+    while not_in_range(run_duration, constraints.run_range):
 
         wait_pre_stim = flexible_values(p.pulse_gap, n_trials, rng)
         gap_dur = flexible_values(p.pulse_gap, n_pulses, rng)
@@ -104,9 +104,9 @@ def generate_run(d, p, rng=None):
     llr_sd = np.inf
     expected_acc = np.inf
 
-    while (not_in_range(llr_mean, d.mean_range)
-           or not_in_range(llr_sd, d.sd_range)
-           or not_in_range(expected_acc, d.acc_range)):
+    while (not_in_range(llr_mean, constraints.mean_range)
+           or not_in_range(llr_sd, constraints.sd_range)
+           or not_in_range(expected_acc, constraints.acc_range)):
 
         for i in [0, 1]:
             dist = "norm", p.dist_means[i], p.dist_sds[i]
@@ -122,7 +122,7 @@ def generate_run(d, p, rng=None):
         llr_sd = target_llr.std()
 
         dv = pd.Series(target_llr).groupby(pd.Series(trial)).sum()
-        dv_sd = np.sqrt(d.sigma ** 2 * pulse_count)
+        dv_sd = np.sqrt(constraints.sigma ** 2 * pulse_count)
         expected_acc = stats.norm(dv, dv_sd).sf(0).mean()
 
     # --- Build the pulse_info structure
@@ -176,7 +176,6 @@ def compute_llr(c, p):
     m0, m1 = p.dist_means
     s0, s1 = p.dist_sds
     d0, d1 = stats.norm(m0, s0), stats.norm(m1, s1)
-
     l0, l1 = np.log10(d0.pdf(c)), np.log10(d1.pdf(c))
     llr = l1 - l0
     return llr
@@ -191,11 +190,11 @@ if __name__ == "__main__":
     p = Bunch(params.scan)
 
     # Design constraints
-    d = Bunch(
+    constraints = Bunch(
 
         trials_per_run=20,
 
-        max_stim_repeat=3,
+        max_stim_repeat=3,  # TODO change to 2?
         max_dist_repeat=4,
 
         sum_count_error=3,
@@ -214,6 +213,6 @@ if __name__ == "__main__":
 
     for i in range(n_designs):
 
-        trials, pulses = generate_run(d, p)
+        trials, pulses = generate_run(constraints, p)
         trials.to_csv("designs/trials_{:03d}.csv".format(i), index=False)
         pulses.to_csv("designs/pulses_{:03d}.csv".format(i), index=False)
