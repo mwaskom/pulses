b35bb41bc7752d4b4286fa23e7cdcab3cd1ad11b
Michael Waskom <mwaskom@nyu.edu>
2018-10-08 17:03:08 -0400

    Fix demo mode part 2

--- fmri/experiment.py
+++ fmri/experiment.py
@@ -578,15 +578,15 @@ def demo_mode(exp):
         exp.draw(["fix", "targets", "cue"])
 
     for frame in exp.frame_range(seconds=exp.p.pulse_dur):
-        exp.draw(["pattern", "fix"])
+        exp.draw(all_stims)
 
-    exp.wait_until("space", draw="fix", check_abort=True)
+    exp.wait_until("space", draw=["fix" "targets", "cue"], check_abort=True)
 
     exp.s.pattern.contrast = 10 ** (exp.p.dist_means[1] + exp.p.dist_sds[1])
-    exp.wait_until("space", draw=["pattern", "fix"], check_abort=True)
+    exp.wait_until("space", draw=all_stims, check_abort=True)
 
     exp.s.pattern.contrast = 10 ** (exp.p.dist_means[0] - exp.p.dist_sds[0])
-    exp.wait_until("space", ["pattern", "fix"], check_abort=True)
+    exp.wait_until("space", draw=all_stims, check_abort=True)
 
     exp.wait_until("space", draw="fix", check_abort=True)
 
