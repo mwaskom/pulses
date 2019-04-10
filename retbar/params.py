62842162efb6d9470b0f2b5f2ba72601d6bf23c5
Michael Waskom <mwaskom@nyu.edu>
2019-04-10 15:48:28 -0400

    Commenting some parameters

--- retbar/params.py
+++ retbar/params.py
@@ -1,32 +1,54 @@
 base = dict(
 
+    # Display characterisics
     display_name="mlw-mbpro",
     display_luminance=50,
+    monitor_eye=True,
 
+    # Size of the fixation point
     fix_radius=.15,
 
-    fix_bar_color=(.8, .6, -.8),
-    fix_fix_color=(.8, .6, -.8),
-    fix_odd_color=(.45, .28, -.85),
+    # Colors to cycle through and distribution for timing the changes
+    fix_colors=['#86ade2', '#70bc83'],
+    fix_duration=("truncexpon", (12 - 4) / 4, 4, 4),
 
+    # Full width (or height) of the mappable area, in degrees
     field_size=24,
+
+    # Duration of each bar step, in seconds
     step_duration=1.5,
 
-    bar_width=8 / 3.,
+    # True if the full width of the bar is visible at the edges
+    full_edges=False,  # True,
 
+    # Width of the bar, in degrees
+    bar_width=3,  # 8 / 3.,
+
+    # Parameters of the constituent gabors in the bar
     element_size=2,
     element_tex="sin",
     element_mask="gauss",
 
+    # Luminance or chromatic contrast of the gabors
+    contrast=1,
+
     sf_distr=("truncexpon", 3.5 / 2, .5, 2),
     prop_color=.5,
 
+    # Rate at which new elements are drawn, in Hz
     update_rate=2,
+
+    # Rate at which the gabors drift, in cycles per second
     drift_rate=.5,
 
-    key="space",
+    # Acceptable keys to indicate fixation change detection
+    resp_keys=["space"],
 
-    output_template="data/{subject}/{session}/oddbar_{time}",
+    # Delay after change for which responses will count as a hit
+    resp_thresh=1,
+
+    # How to save the output data
+    output_template="data/{subject}/{session}/retbar_{time}",
 
 )
 
@@ -34,7 +56,7 @@ train = base.copy()
 train.update(
 
     display_name="kianilab-ps1",
-    monitor_eye=True,
+    contrast=.9,
 )
 
 
@@ -43,9 +65,16 @@ scan.update(
 
     display_name="nyu-cbi-propixx",
     eye_host_address="192.168.1.5",
-    key="1",
-    monitor_eye=True,
     trigger=["5"],
     pre_trigger_stim="fix",
+
+    resp_keys=["1", "2", "3", "4", "6", "7", "8", "9"],
+
+    # TODO check that contrast of 1 is ok at the scanner
+    contrast=1,
+
+    # Aperture to avoid bright screen spilling over bore screen edges
+    aperture_radius=19,
+    aperture_center=(0, -7.2),
 
 )
